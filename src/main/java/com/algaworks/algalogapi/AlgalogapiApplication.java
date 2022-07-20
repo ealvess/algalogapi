5f244a0bbce51e64d38a7b16e5c0b06c4733b959
ealvess <everton10alves@gmail.com>
2022-07-20 18:40:29 -0300

    Configurando Flyway, Usando Jakarta Persistence (JPA) e usando Spring Data JPA

---
+++ src/main/java/com/algaworks/algalogapi/AlgalogapiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algalogapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgalogapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgalogapiApplication.class, args);
+	}
+
+}
